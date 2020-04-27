b6a6fc44a42faaf07dd2137f368f93ed97ec75c6
Victor de Baare <victor.de.baare@gmail.com>
2020-04-27 15:02:13 +0200

    Tobasco core initial

---
+++ TobascoCore/CodeGeneration/CodeGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Tobasco.CodeGeneration
+{
+    public class CodeGenerator : Task
+    {
+        [Output]
+        public ITaskItem[] GeneratedFiles { get; private set; }
+
+        public override bool Execute()
+        {
+            GeneratedFiles = new List<ITaskItem> { new TaskItem { ItemSpec = "Test" } }.ToArray();
+            return true;
+        }
+    }
+}
